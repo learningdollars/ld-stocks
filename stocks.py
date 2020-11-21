299d9b6f7054275a3b87034a87dda05d898e01f6
Gobi Dasu <gobi@gobidasu.com>
2020-11-21 17:39:38 -0600

    Update stocks.py

--- stocks.py
+++ stocks.py
@@ -7,27 +7,30 @@ rate = []
 # function
 def csv_url_reader(url_obj):
     reader = csv.DictReader(url_obj,delimiter = ',')
+    browser = webdriver.Chrome()
     for line in reader:
-        url = line["URL"]
-        title = line["Title"]
-        # print(url +' ' + title)
-        browser = webdriver.Chrome()
-        browser.get(url)
-        stockname = browser.find_elements_by_css_selector('header.Jo5RGrWjFiX_iyW3gMLsy')
-        for stock in stockname:
-            name = stock.find_element_by_css_selector('h1').text
-            stock_name.append(name)
-        ratings = browser.find_elements_by_css_selector('div._1fEdz1YPOLpLW1Ow3rKh92')
-        for rating in ratings:
-            rat = rating.find_element_by_css_selector('div._1fEdz1YPOLpLW1Ow3rKh92 h2').text
-            print(rat)
-            if rat == ' ':
-                print('NA')
-            rate.append(rat)
-        assert title in browser.title
-        df = pd.DataFrame(list(zip(stock_name,rate)), columns=['Stock_Name','Rating'])
-        df.to_csv('Final.csv',index=False)
-        browser.quit()
+        try:
+            url = line["URL"]
+            title = line["Title"]
+            # print(url +' ' + title)
+            browser.get(url)
+            stockname = browser.find_elements_by_css_selector('header.Jo5RGrWjFiX_iyW3gMLsy')
+            for stock in stockname:
+                name = stock.find_element_by_css_selector('h1').text
+                stock_name.append(name)
+            ratings = browser.find_elements_by_css_selector('div._1fEdz1YPOLpLW1Ow3rKh92')
+            for rating in ratings:
+                rat = rating.find_element_by_css_selector('div._1fEdz1YPOLpLW1Ow3rKh92 h2').text
+                print(rat)
+                if rat == ' ':
+                    print('NA')
+                rate.append(rat)
+            assert title in browser.title
+            df = pd.DataFrame(list(zip(stock_name,rate)), columns=['Stock_Name','Rating'])
+            df.to_csv('Final.csv',index=False)
+        except:
+            print("NA")
+    browser.quit()
 
 
 if __name__ =="__main__":
