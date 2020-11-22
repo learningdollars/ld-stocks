7f281e7ad601da4a5e651c3d99f77ce5644369e5
noslen254 <psammy.ngei@gmail.com>
2020-11-22 11:23:21 +0300

    update

--- stocks.py
+++ stocks.py
@@ -11,9 +11,7 @@ markets = []
 # function
 def csv_url_reader(url_obj):
     reader = csv.DictReader(url_obj,delimiter = ',')
-    browser = webdriver.Chrome()
     for line in reader:
-<<<<<<< HEAD
         # url = line["URL"]
         precision = line["Predecision"]
         title = line["Stock"]
@@ -21,13 +19,6 @@ def csv_url_reader(url_obj):
         if precision =='TRUE':
             browser = webdriver.Chrome()
             browser.get("https://robinhood.com/stocks/" + title)
-=======
-        try:
-            url = line["URL"]
-            title = line["Title"]
-            # print(url +' ' + title)
-            browser.get(url)
->>>>>>> 299d9b6f7054275a3b87034a87dda05d898e01f6
             stockname = browser.find_elements_by_css_selector('header.Jo5RGrWjFiX_iyW3gMLsy')
             for stock in stockname:
                 name = stock.find_element_by_css_selector('h1').text
@@ -35,7 +26,6 @@ def csv_url_reader(url_obj):
             ratings = browser.find_elements_by_css_selector('div._1fEdz1YPOLpLW1Ow3rKh92')
             for rating in ratings:
                 rat = rating.find_element_by_css_selector('div._1fEdz1YPOLpLW1Ow3rKh92 h2').text
-<<<<<<< HEAD
                 # print(rat + ' '+ precision)
                 rate.append(rat)
             # assert title in browser.title
@@ -61,20 +51,3 @@ def csv_url_reader(url_obj):
 if __name__ =="__main__":
     with open("sheet1.csv") as url_obj:
         csv_url_reader(url_obj)
-=======
-                print(rat)
-                if rat == ' ':
-                    print('NA')
-                rate.append(rat)
-            assert title in browser.title
-            df = pd.DataFrame(list(zip(stock_name,rate)), columns=['Stock_Name','Rating'])
-            df.to_csv('Final.csv',index=False)
-        except:
-            print("NA")
-    browser.quit()
-
-
-if __name__ =="__main__":
-    with open("tickers.csv") as url_obj:
-        csv_url_reader(url_obj)
->>>>>>> 299d9b6f7054275a3b87034a87dda05d898e01f6
