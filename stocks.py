e33d8ffc1e89a83044d871bb395799cf882255c4
noslen254 <psammy.ngei@gmail.com>
2020-11-24 10:36:27 +0300

    last changes

--- stocks.py
+++ stocks.py
@@ -5,10 +5,6 @@ import time
 from selenium import webdriver
 
 import pandas as pd
-stock_name = []
-rate = []
-guru_value = []
-markets = []
 # function
 def csv_url_reader(url_obj):
     reader = csv.DictReader(url_obj,delimiter = ',')
@@ -22,13 +18,16 @@ def csv_url_reader(url_obj):
             browser.get("https://robinhood.com/stocks/" + title)
             stockname = browser.find_elements_by_css_selector('header.Jo5RGrWjFiX_iyW3gMLsy')
             for stock in stockname:
-                name = stock.find_element_by_css_selector('h1').text
-                stock_name.append(name)
+                try:
+                    name = stock.find_element_by_css_selector('h1').text
+                except:
+                    ('Oops!!',sys.exc_info)
             ratings = browser.find_elements_by_css_selector('div._1fEdz1YPOLpLW1Ow3rKh92')
             for rating in ratings:
-                rat = rating.find_element_by_css_selector('div._1fEdz1YPOLpLW1Ow3rKh92 h2').text
-                # print(rat + ' '+ precision)
-                rate.append(rat)
+                try:
+                    rat = rating.find_element_by_css_selector('div._1fEdz1YPOLpLW1Ow3rKh92 h2').text
+                except:
+                    ('Oops!!',sys.exc_info())
             # assert title in browser.title
             # Market Technology
             try:
@@ -42,18 +41,26 @@ def csv_url_reader(url_obj):
             for guru in gurus:
                 try:
                     data = guru.find_element_by_css_selector("span").text
-                    guru_value.append(data)
                 except:
                     ('Oops!',sys.exc_info())
             # Output
-            print((name + ' '+ rat  +' ' + data + ' ' + str(tech)))
-            df = pd.DataFrame(list(zip(stock_name,rate,guru_value,markets)), columns=['Stock_Name','Rating','Guru_value','Market'])
-            df.to_csv('Final.csv',index=False)
+            browser.get("https://markets.businessinsider.com/stocks/"+title+"-stock")
+            earnings = browser.find_elements_by_class_name("price-section__row")
+            for earning in earnings:
+                try:
+                    title = earning.find_element_by_css_selector("span.price-section__current-value").text
+                    if title > "1":
+                        title = "YES"
+                    else:
+                        title = "NO"
+                except:
+                    ('Oops',sys.exc_info())
+            print((name + ', '+ rat  +', ' + data + ', ' + str(tech) + ', ' + title))
             browser.quit()
             time.sleep(2)
         else:
             print("NA")
 
 if __name__ =="__main__":
-    with open("tickers.csv") as url_obj:
+    with open("tickers1.csv") as url_obj:
         csv_url_reader(url_obj)
--- yf.py
+++ yf.py
@@ -1,68 +1,18 @@
 from selenium import webdriver
-import csv
+import sys
 
 browser = webdriver.Firefox()
-browser.get("https://www.gurufocus.com/stock/AAPL/summary")
-
-markets = browser.find_elements_by_css_selector('button.el-button.fs-regular.el-button--danger.el-button--mini.el-popover__reference')
-for market in markets:
-    data = market.find_element_by_css_selector('span').text
-    print(data)
-    browser.quit()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# # import yfinance as yf
-# # import sys
-# # data = yf.Ticker("FB")
-# # tex = data.info.get('sector')
-# # if tex == 'None':
-# #     try:
-# #         ('Oops',sys.exc_info())
-# #     except:
-# #         print(tex)
-# from selenium import webdriver
-# import csv
-# import sys
-# datas = []
-# import yfinance as yf
-# def open_yahoo(url_obj):
-#     yf.pdr_override()
-#     reader = csv.DictReader(url_obj,delimiter = ',')
-#     for line in reader:
-#         title = line["Stock"]
-#         browser = webdriver.Chrome()
-#         browser.get("https://www.gurufocus.com/stock/"+title+"/summary")
-#         gurus = browser.find_elements_by_css_selector("button.el-button.fs-regular.el-button--danger.el-button--mini.el-popover__reference")
-#         for guru in gurus:
-#             data = guru.find_element_by_css_selector("span").text
-#             datas.append(data)
-#             # print(data)
-#         try:
-#             title = line["Stock"]
-#             data = yf.Ticker(title)
-#             mar = (data.info.get('sector'))
-#             print(mar + str(data))
-#         except:
-#             print('Oops!',sys.exc_info())
-#             print()
-
-# if __name__ =="__main__":
-#     with open("tickers1.csv") as url_obj:
-#         open_yahoo(url_obj)
+browser.get("https://markets.businessinsider.com/stocks/msft-stock")
+earnings = browser.find_elements_by_class_name("price-section__row")
+for earning in earnings:
+    try:
+        title = earning.find_element_by_css_selector("span.price-section__current-value").text
+        if title > "1":
+            title = "YES"
+        else:
+            title = "NO"
+        browser.quit()
+    except:
+        ("Oops",sys.exc_info)
+    
+    print(title)
