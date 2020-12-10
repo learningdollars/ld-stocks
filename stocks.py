d29fd20deca5c1851e463ed4309af8f135d414d2
noslen254 <psammy.ngei@gmail.com>
2020-12-10 10:08:17 +0300

    changes

--- stocks.py
+++ stocks.py
@@ -11,14 +11,10 @@ import pandas as pd
 def csv_url_reader(url_obj):
     reader = csv.DictReader(url_obj,delimiter = ',')
     for line in reader:
-        url = line["URL"]
-        real_ticker = url[43:-6]
-        precision = line["Event"]
-        # print(url +' ' + title)
-        if precision =='Annual General Meeting':
-            print("DONT")
-        else:
-            browser = webdriver.Firefox()
+        real_ticker = line["Stock"]
+        precision = line["Predecision"]
+        if precision =='TRUE':
+            browser = webdriver.Chrome()
             browser.get("https://robinhood.com/stocks/" + real_ticker)
             stockname = browser.find_elements_by_css_selector('header.Jo5RGrWjFiX_iyW3gMLsy')
             for stock in stockname:
@@ -32,38 +28,37 @@ def csv_url_reader(url_obj):
                     rat = rating.find_element_by_css_selector('div._1fEdz1YPOLpLW1Ow3rKh92 h2').text
                 except:
                     ('Oops!!',sys.exc_info())
-            # assert title in browser.title
-            # Market Technology
-            # try:
-            market_tech = yf.Ticker(real_ticker)
-            tech = market_tech.info.get('sector')
-            # print(tech)
-            # except:
-            #     ('Oops!',sys.exc_info())
-            # Getting Guru multiple sites
+            try:
+                market_tech = yf.Ticker(real_ticker)
+                tech = market_tech.info.get('sector')
+            except:
+                (sys.exc_info())
             browser.get("https://www.gurufocus.com/stock/"+real_ticker+"/summary")
             gurus = browser.find_elements_by_css_selector("button.el-button.fs-regular.el-button--danger.el-button--mini.el-popover__reference")
+            data ="UNK"
             for guru in gurus:
                 try:
                     data = guru.find_element_by_css_selector("span").text
                 except:
                     ('Oops!',sys.exc_info())
-            # Output
-            # browser.get("https://markets.businessinsider.com/stocks/"+title+"-stock")
-            # earnings = browser.find_elements_by_class_name("price-section__row")
-            # for earning in earnings:
-            #     try:
-            #         title = earning.find_element_by_css_selector("span.price-section__current-value").text
-            #         if title > "1":
-            #             title = "YES"
-            #         else:
-            #             title = "NO"
-            #     except:
-            #         ('Oops',sys.exc_info())
-            print(name + ', '+ rat  +', ' + data + ', ' + tech)
+            try:
+                browser = webdriver.Chrome()
+                browser.get("https://finance.yahoo.com/calendar/earnings")
+                titles = browser.find_elements_by_css_selector('table tbody tr')
+                for title in titles:
+                    earning = title.find_element_by_css_selector('table tbody tr td a').text
+                    if earning not in real_ticker:
+                        earning = "NO"
+                    else:
+                        earning ="YES"
+            except:
+                (sys.exc_info())
+            print(name + ', '+ rat  +', ' + data + ', ' + tech + ', '+ earning)
             browser.quit()
             time.sleep(2)
+        else:
+            print("NA")
 
 if __name__ =="__main__":
-    with open("Final.csv") as url_obj:
+    with open("tickers1.csv") as url_obj:
         csv_url_reader(url_obj)
