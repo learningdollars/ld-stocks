4cdcff0f344ff2be9719c01281827a31a6e93b27
Gobi Dasu <gobi@gobidasu.com>
2021-05-02 19:43:21 -0500

    finished existing limit sell adjustments, now move onto new limit sells

--- set_limits.py
+++ set_limits.py
@@ -3,7 +3,6 @@ import time
 from selenium.webdriver.support.ui import Select
 
 # constants
-TRADE = "https://client.schwab.com/Areas/Trade/Stocks/Entry.aspx?Symbol="
 WKLY_GROWTH_EXPECTATION = 1.0072
 
 browser = webdriver.Chrome()
@@ -26,6 +25,7 @@ for equity in equities:
 browser.get("https://client.schwab.com/Trade/OrderStatus/ViewOrderStatus.aspx?ViewTypeFilter=All")
 cancels = browser.find_elements_by_class_name("link-cancel.button-secondary.rightClickDisable")
 for i in len(cancels):
+	browser.get("https://client.schwab.com/Trade/OrderStatus/ViewOrderStatus.aspx?ViewTypeFilter=All")
 	cancel = browser.find_elements_by_class_name("link-cancel.button-secondary.rightClickDisable")[i]
 	cancel.find_element_by_xpath('..')
 	whole_limit_call = cancel.find_element_by_xpath('../../../..')
@@ -33,12 +33,32 @@ for i in len(cancels):
 	limit_price = float(whole_limit_call.find_elements_by_tag_name("td")[2].text.split(" ")[1].replace("$", "").replace(",", ""))
 	relevant_ticker = whole_limit_call.get_attribute('innerHTML').split("Symbol=")[1].split('&amp')[0]
 	print("need to adjust limit for", relevant_ticker, num_limit_sell, "shares, curr limit", limit_price)
+	
 	cancel.click()
 	time.sleep(3)
 	confirmation = browser.find_element_by_link_text("Cancel Order")
 	confirmation.click()
 	time.sleep(3)
 	close.click()
+	
+	browser.get("https://client.schwab.com/Areas/Trade/Stocks/Entry.aspx?Symbol="+relevant_ticker)
+	action = Select(browser.find_element_by_id("ddlAction_0"))
+	action.select_by_visible_text("Sell")
+	quantity_input = browser.find_element_by_id("txtQty_0")
+	quantity_input.send_keys(str(num_limit_sell))
+	ordertype = Select(browser.find_element_by_id("ddlType_0"))
+	ordertype.select_by_visible_text("Limit")
+	limitamt_input = browser.find_element_by_id("txtLimit_0")
+	limitamt_input.send_keys(str(limit_price*WKLY_GROWTH_EXPECTATION))
+	timing = Select(browser.find_element_by_id("ddlTiming_0"))
+	timing.select_by_visible_text("Good Until Canceled")
+	review_order = browser.find_element_by_id("btnReview")
+	review_order.click()
+	time.sleep(3)
+	confirm = browser.find_element_by_id("btnConfirm")
+	confirm.click()
+
+	owned_equities[ticker]["number_limit_adjusted"] += 1
 
 
 # minimize dependencies
@@ -72,8 +92,7 @@ for stock in f:
 	print("==== setting initial limit for ", stock)
 	browser.get(TRADE + ticker)
 	
-	action = Select(browser.find_element_by_id("ddlAction_0"))
-	action.select_by_visible_text("Sell")
+	
 	
 	# need to grab the quantity owned right now -- see above
 
@@ -81,24 +100,13 @@ for stock in f:
 	
 
 	# need to find the difference - that's the amount we need to initially limit sell
+		
 	
-	quantity_to_limit_sell = 1
-	print(quantity_to_limit_sell, "shares")
-	quantity_input = browser.find_element_by_id("txtQty_0")
-	quantity_input.send_keys(str(quantity_to_limit_sell))
-	
-	ordertype = Select(browser.find_element_by_id("ddlType_0"))
-	ordertype.select_by_visible_text("Limit")
 
 	# need to grab the cost we bought the stock for
-	purchase_price = 3341
 	
-	initial_limit = round(purchase_price*INITIAL_GROWTH, 2)
-	limitamt_input = browser.find_element_by_id("txtLimit_0")
-	limitamt_input.send_keys(str(initial_limit))
 
-	review_order = browser.find_element_by_id("btnReview")
-	review_order.click()
+	
 
 	print("==== changing limit for ", stock)
 	# check how many shares for which we have already set a limit sell -- check the bought.csv
